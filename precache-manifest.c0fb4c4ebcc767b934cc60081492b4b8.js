a43e9ffd929243b5fb750ce3482dde34f8beaec0
Kah919 <kahyap13@gmail.com>
2020-06-08 16:26:24 -0400

    Updates

---
+++ precache-manifest.c0fb4c4ebcc767b934cc60081492b4b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b2488573854f5ba5cd728f83eb9504",
+    "url": "/MintyColors/index.html"
+  },
+  {
+    "revision": "e9305cdcbce75cfbd7db",
+    "url": "/MintyColors/static/css/main.a10074d2.chunk.css"
+  },
+  {
+    "revision": "a40bd14d74d74586192a",
+    "url": "/MintyColors/static/js/2.2ae74d28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MintyColors/static/js/2.2ae74d28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9305cdcbce75cfbd7db",
+    "url": "/MintyColors/static/js/main.691f13e7.chunk.js"
+  },
+  {
+    "revision": "30f66882ffaaa36004d0",
+    "url": "/MintyColors/static/js/runtime-main.9f33a4f0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/minty-skittles/precache-manifest.3c6334067b66f6939ea70f2256b628fb.js"
+  "/MintyColors/precache-manifest.c0fb4c4ebcc767b934cc60081492b4b8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/minty-skittles/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MintyColors/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
