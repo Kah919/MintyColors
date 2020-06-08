cd9637bf4b6320cd038c6188c87a60d670c9166f
Kah919 <kahyap13@gmail.com>
2020-06-08 16:24:37 -0400

    Updates

---
+++ precache-manifest.3c6334067b66f6939ea70f2256b628fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5beb3a8182a55948ae5654fb0ddf1c9b",
+    "url": "/minty-skittles/index.html"
+  },
+  {
+    "revision": "9ffa4a0a685778723864",
+    "url": "/minty-skittles/static/css/main.a10074d2.chunk.css"
+  },
+  {
+    "revision": "a40bd14d74d74586192a",
+    "url": "/minty-skittles/static/js/2.2ae74d28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minty-skittles/static/js/2.2ae74d28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ffa4a0a685778723864",
+    "url": "/minty-skittles/static/js/main.59318894.chunk.js"
+  },
+  {
+    "revision": "213fd8aa9ca08b21718a",
+    "url": "/minty-skittles/static/js/runtime-main.c51c59e8.js"
+  }
+]);
