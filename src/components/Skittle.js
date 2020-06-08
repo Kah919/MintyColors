23a64f44c316485bbbdf49a2b688dd50f7ddc899
Kah919 <kahyap13@gmail.com>
2020-06-08 15:50:00 -0400

    Skittles changes color scheme every second

--- src/components/Skittle.js
+++ src/components/Skittle.js
@@ -1,11 +1,52 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
+import ColorScheme from 'color-scheme';
+const useInterval = (callback, delay) => {
+    const savedCallback = React.useRef();
+  
+    React.useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
+  
+    React.useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+      if (delay !== null) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+  };
 
 export default () => {
-    const [color, setColor] = useState('');
+    const [colorArray, setColorArray] = useState([]);
+    let [curr, setCurr] = useState(0);
+    const [shouldChange, setShouldChange] = useState(true);
+
+    useEffect(() => {
+        let scheme = new ColorScheme;
+        scheme.from_hue(21).scheme('triade').variation('soft');
+
+        let color = scheme.colors();
+        setColorArray(color);
+        setShouldChange(false);
+
+    }, shouldChange);
+    
+    useInterval(() => {
+        const end = colorArray.length - 1;
+        if(curr !== end) {
+            setCurr(curr++);
+        } else {
+            setCurr(0);
+        }
+    }, 500)
+
+    console.log(colorArray)
+
     return(
         <div className="skittle_container">
-            <div className="skittle"></div>
-            
+            <div className="skittle" style={{backgroundColor: `#${colorArray[curr]}`}}></div>
         </div>
     )
 }
